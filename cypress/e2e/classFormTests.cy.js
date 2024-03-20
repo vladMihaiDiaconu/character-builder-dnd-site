ad9de4e18167f2cbb3398df12656443c936d66f7
vladMihaiDiaconu <113431038+vladMihaiDiaconu@users.noreply.github.com>
2024-03-20 21:11:46 +0000

    Added character stats form and styling changes to fit an additional form

---
+++ cypress/e2e/classFormTests.cy.js
@@ -0,0 +1,33 @@
+describe('Create New Character Page', () => {
+    beforeEach(() => {
+      cy.visit('http://localhost:4321/create-new-character');
+      cy.get('#progress-2').click();
+    });
+  
+    it('has correct options for class select', () => {
+      cy.get('#progress-2-class').then(select => {
+        const actualOptions = [...select[0].options].map(option => option.text);
+        const expectedOptions = ['Select','Bard'];
+        expect(actualOptions).to.deep.eq(expectedOptions);
+      });
+    });
+
+    it('displays the corresponding div when a class is selected', () => {
+        const classValue = 'Bard';        
+        cy.get('#progress-2-class').select(classValue);
+        cy.get(`#${classValue}`).should('be.visible');
+        cy.get(`#${classValue}`).should('not.have.attr', 'hidden');
+    });
+
+    // it('correctly toggles visibility when switching between two races', () => {
+    //     const options = [
+    //       { value: 'DarkElf', expectedToShow: 'DarkElf', expectedToHide: 'HighElf' },
+    //       { value: 'HighElf', expectedToShow: 'HighElf', expectedToHide: 'DarkElf' }
+    //     ];
+    //     options.forEach(option => {
+    //       cy.get('#progress-1-race').select(option.value);
+    //       cy.get(`#${option.expectedToShow}`).should('be.visible').and('not.have.attr', 'hidden');
+    //       cy.get(`#${option.expectedToHide}`).should('not.be.visible');
+    //     });
+    // });
+});
