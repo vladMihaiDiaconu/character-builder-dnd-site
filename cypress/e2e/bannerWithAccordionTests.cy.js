1e8f110c8c0706786836b8e38aaaef7e94ba3599
vladMihaiDiaconu <113431038+vladMihaiDiaconu@users.noreply.github.com>
2024-03-20 18:46:08 +0000

    Added logic to support switching between forms

--- cypress/e2e/bannerWithAccordionTests.cy.js
+++
@@ -1,7 +0,0 @@
-import BannerWithAccordion from "../../src/components/banners/BannerWithAccordion.astro"
-
-it('Banner With Accordion Component progress buttons working', () => {
-cy.mount(<BannerWithAccordion />)
-
-console.log(cy.get('button'));
-})
--- cypress/e2e/raceFormTests.cy.js
+++ cypress/e2e/raceFormTests.cy.js
@@ -1,8 +1,32 @@
-import RaceForm from "../../src/components/character-creation-forms/RaceForm.astro";
-import data from "../mock-data/races.json"
+describe('Create New Character Page', () => {
+    beforeEach(() => {
+      cy.visit('http://localhost:4321/create-new-character');
+    });
+  
+    it('has correct options for race select', () => {
+      cy.get('#progress-1-race').then(select => {
+        const actualOptions = [...select[0].options].map(option => option.text);
+        const expectedOptions = ['Select','High Elf', 'Dark Elf'];
+        expect(actualOptions).to.deep.eq(expectedOptions);
+      });
+    });
 
-it('Race Form Component filter working', () => {
-cy.mount(<RaceForm races={data}/>)
+    it('displays the corresponding div when a race is selected', () => {
+        const raceValue = 'HighElf';        
+        cy.get('#progress-1-race').select(raceValue);
+        cy.get(`#${raceValue}`).should('be.visible');
+        cy.get(`#${raceValue}`).should('not.have.attr', 'hidden');
+    });
 
-console.log(cy.get('select'));
-})
+    it('correctly toggles visibility when switching between two races', () => {
+        const options = [
+          { value: 'DarkElf', expectedToShow: 'DarkElf', expectedToHide: 'HighElf' },
+          { value: 'HighElf', expectedToShow: 'HighElf', expectedToHide: 'DarkElf' }
+        ];
+        options.forEach(option => {
+          cy.get('#progress-1-race').select(option.value);
+          cy.get(`#${option.expectedToShow}`).should('be.visible').and('not.have.attr', 'hidden');
+          cy.get(`#${option.expectedToHide}`).should('not.be.visible');
+        });
+    });
+});
